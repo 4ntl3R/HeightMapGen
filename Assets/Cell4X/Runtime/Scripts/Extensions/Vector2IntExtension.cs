fba4d97337bb0a875439b66182cd030876edbae5
4ntl3R <71680575+4ntl3R@users.noreply.github.com>
2022-12-05 23:26:32 +0600

    Added Vector2Int extension

---
+++ Assets/Cell4X/Runtime/Scripts/Extensions/Vector2IntExtension.cs
@@ -0,0 +1,39 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Cell4X.Runtime.Scripts.Extensions
+{
+    public static class Vector2IntExtension
+    {
+        public static List<Vector2Int> GetAdjacent(this Vector2Int target, Vector2Int arrayLength)
+        {
+            var result = new List<Vector2Int>
+            {
+                new Vector2Int(target.x, target.y - 1),
+                new Vector2Int(target.x, target.y + 1),
+                new Vector2Int(target.x - 1, target.y),
+                new Vector2Int(target.x + 1, target.y)
+            };
+
+            return result.FindAll(x => x.IsValid(arrayLength));
+        }
+        
+        public static List<Vector2Int> GetDiagonal(this Vector2Int target, Vector2Int arrayLength)
+        {
+            var result = new List<Vector2Int>
+            {
+                new Vector2Int(target.x - 1, target.y - 1),
+                new Vector2Int(target.x + 1, target.y + 1),
+                new Vector2Int(target.x - 1, target.y + 1),
+                new Vector2Int(target.x + 1, target.y - 1)
+            };
+
+            return result.FindAll(x => x.IsValid(arrayLength));
+        }
+
+        private static bool IsValid(this Vector2Int target, Vector2Int arrayLength)
+        {
+            return (target.x >= 0) && (target.y >= 0) && (target.x < arrayLength.x) && (target.y < arrayLength.y);
+        }
+    }
+}
