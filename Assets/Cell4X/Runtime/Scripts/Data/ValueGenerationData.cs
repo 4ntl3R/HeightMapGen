4ac4dde7e6cf732b9a1ad954c07a103447940878
4ntl3R <71680575+4ntl3R@users.noreply.github.com>
2022-12-07 15:41:10 +0600

    Added ValueGenerationaData
    Added value generation data scriptable object

---
+++ Assets/Cell4X/Runtime/Scripts/Data/ValueGenerationData.cs
@@ -0,0 +1,55 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+namespace Cell4X.Runtime.Scripts.Data
+{
+    [CreateAssetMenu(order = 0, fileName = "ValueGenerationData", menuName = "Cell4X/ValueGenerationData"), ]
+    public class ValueGenerationData<T> : ScriptableObject
+    {
+        [SerializeField] 
+        private List<SerializablePair<T, int>> valueAndRelativeFrequency;
+
+        [SerializeField] 
+        private List<float> borderValues;
+
+        private bool _isInitiated = false;
+
+        public T GetRandomValue()
+        {
+            if (!_isInitiated)
+            {
+                Initiate();
+                _isInitiated = true;
+            }
+
+            return valueAndRelativeFrequency[FindIndex(Random.value)].First;
+        }
+
+        private void Initiate()
+        {
+            var frequencySum = valueAndRelativeFrequency.Sum(pair => pair.Second);
+            var currentSum = 0;
+            
+            borderValues.Add(0);
+            for (var i = 0; i < valueAndRelativeFrequency.Count; i++)
+            {
+                currentSum += valueAndRelativeFrequency[i].Second;
+                borderValues.Add((float)currentSum/frequencySum);
+            }
+        }
+
+        private int FindIndex(float percentValue)
+        {
+            for (var i = 0; i < valueAndRelativeFrequency.Count - 1; i++)
+            {
+                if (borderValues[i] < percentValue && borderValues[i + 1] > percentValue)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
