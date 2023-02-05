410d0f19bb1c76d46331ad71dc424f0645f8cf37
4ntl3R <71680575+4ntl3R@users.noreply.github.com>
2023-02-06 02:58:42 +0600

    Added parameters

---
+++ Assets/Cell4X/Runtime/Scripts/Enums/ParameterType.cs
@@ -0,0 +1,16 @@
+namespace Cell4X.Runtime.Scripts.Enums
+{
+    public enum ParameterType 
+    {
+        Seed = -1,
+        FieldSize = 0,
+        PlatesNumber = 1,
+        PlatesBorderSize = 2,
+        PlatesHeightRandom = 3,
+        PlateRangeRandom = 4,
+        NoiseLevel = 5,
+        NoiseReduction = 6,
+        NoisePower = 7,
+        SmoothingLevel = 8 
+    }
+}
